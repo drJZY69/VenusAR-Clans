48bd20a5ddf144e70040d708d55a1e9662cfed98
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:48:13 +0300

    Update start-tracking.js

--- commands/start-tracking.js
+++ commands/start-tracking.js
@@ -1,1 +1,20 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import Tracker from "../utils/tracker.js";
 
+export default {
+  name: "start-tracking",
+  data: new SlashCommandBuilder()
+    .setName("start-tracking")
+    .setDescription("تشغيل نظام تتبع النقاط للكلانات")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction, client) {
+    if (Tracker.running) {
+      return interaction.reply({ content: "⚠️ التتبع يعمل بالفعل!", ephemeral: true });
+    }
+
+    Tracker.start(client);
+
+    return interaction.reply("✅ تم تشغيل نظام التتبع بنجاح!");
+  }
+};
