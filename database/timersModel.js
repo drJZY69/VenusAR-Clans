f2a00e34ee0debc3d8e74bd332e574da6a39e0be
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:39:51 +0300

    Update timersModel.js

--- database/timersModel.js
+++ database/timersModel.js
@@ -1,1 +1,8 @@
+import mongoose from "mongoose";
 
+const timerSchema = new mongoose.Schema({
+  clanName: { type: String, required: true },
+  lastTick: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Timer", timerSchema);
