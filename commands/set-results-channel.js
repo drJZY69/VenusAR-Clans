2bd0fa779a7f01ea875ee03a72f99701a5f878d2
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:45:52 +0300

    Update set-results-channel.js

--- commands/set-results-channel.js
+++ commands/set-results-channel.js
@@ -1,1 +1,26 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import fs from "fs";
 
+export default {
+  name: "set-results-channel",
+  data: new SlashCommandBuilder()
+    .setName("set-results-channel")
+    .setDescription("تحديد الروم الذي يرسل فيه البوت النتائج الأسبوعية")
+    .addChannelOption(option =>
+      option.setName("channel")
+        .setDescription("الروم المطلوب")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    const channel = interaction.options.getChannel("channel");
+
+    // تحديث config.json
+    const config = JSON.parse(fs.readFileSync("./config.json"));
+    config.resultsChannel = channel.id;
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
+
+    return interaction.reply(`📢 تم تحديد روم النتائج: <#${channel.id}>`);
+  }
+};
