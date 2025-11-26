d5d58dd10880ea58e67b834f57fe899bb93ee2ea
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:39:40 +0300

    Update recordModel.js

--- database/recordModel.js
+++ database/recordModel.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+const recordSchema = new mongoose.Schema({
+  clanName: { type: String, required: true },
+  topMemberId: { type: String, required: true },
+  points: { type: Number, required: true },
+  period: { type: String, required: true }, // week / month / year
+  date: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Record", recordSchema);
