11ed169680386de7ff5c883329903fc2c6dac3e6
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:48:26 +0300

    Update stop-tracking.js

--- commands/stop-tracking.js
+++ commands/stop-tracking.js
@@ -1,1 +1,19 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import Tracker from "../utils/tracker.js";
 
+export default {
+  name: "stop-tracking",
+  data: new SlashCommandBuilder()
+    .setName("stop-tracking")
+    .setDescription("إيقاف نظام التتبع")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    if (!Tracker.running) {
+      return interaction.reply({ content: "⚠️ التتبع متوقف بالفعل!", ephemeral: true });
+    }
+
+    Tracker.stop();
+    return interaction.reply("🛑 تم إيقاف نظام التتبع.");
+  }
+};
