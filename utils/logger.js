f9e24f84c896d77569654961347d1490ebdf44da
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:50:14 +0300

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,18 @@
+export default {
+  info(msg) {
+    console.log(`ℹ️ INFO: ${msg}`);
+  },
+
+  success(msg) {
+    console.log(`✅ SUCCESS: ${msg}`);
+  },
+
+  warning(msg) {
+    console.log(`⚠️ WARNING: ${msg}`);
+  },
+
+  error(msg) {
+    console.log(`❌ ERROR: ${msg}`);
+  }
+};
 
