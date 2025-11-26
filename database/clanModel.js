ad44bd53d44d7c1ced73c8c111975026f6319b81
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:39:16 +0300

    Update clanModel.js

--- database/clanModel.js
+++ database/clanModel.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const clanSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  roleId: { type: String, required: true },
+  voiceChannelId: { type: String, required: true },
+  timer: { type: Number, default: 30 }, // بالدقائق
+  totalPoints: { type: Number, default: 0 }, // مجموع نقاط الكلان
+  membersCount: { type: Number, default: 0 }  // عدد اعضاء الكلان المسجلين
+});
+
+export default mongoose.model("Clan", clanSchema);
