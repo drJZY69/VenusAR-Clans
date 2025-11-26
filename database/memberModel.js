da3940be32aa31c4723310ed86f58c8df4e33702
drJZY69 <drJZY599@gmail.com>
2025-11-27 00:39:27 +0300

    Update memberModel.js

--- database/memberModel.js
+++ database/memberModel.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const memberSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  clanName: { type: String, required: true },
+  weeklyPoints: { type: Number, default: 0 },
+  monthlyPoints: { type: Number, default: 0 },
+  yearlyPoints: { type: Number, default: 0 },
+  totalPoints: { type: Number, default: 0 }
+});
+
+export default mongoose.model("Member", memberSchema);
